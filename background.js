351586ab7d87ddc5e408c52a96771da6508005a7
secf4ult <secf4ult@outlook.com>
2024-09-25 11:26:26 +0900

    feat: add background job

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+// YouTube 'watch' page won't run contentScript.js if you clicked another video there,
+// so there has to be a background job to monitor the url change
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   const isCompleted = changeInfo.status === 'complete';
   const isTargetUrl = tab.url && tab.url.includes('youtube.com/watch');
